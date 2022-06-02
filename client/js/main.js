9cee52063989563c704a8f7374ec110adf868571
Sole Rocha <soledadrocha@outlook.com.ar>
2022-06-02 15:22:17 -0300

    Primer commit.

---
+++ client/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	$('.btt-up').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.btt-up').slideDown(300);
+		} else {
+			$('.btt-up').slideUp(300);
+		}
+	});
+
+});
